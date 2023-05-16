233a9c0f580fd3e934bed5768951d8bdc006da22
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-16 16:41:32 +0200

    add new file

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php
 
+class Genre
+{
+    public $generi;
+};
 
 class Movie
 {
+    public static $Type = 'Film';
     public $title;
     public $genre;
     public $runtime;
     public $actors;
     public $description;
 
-    public function __construct($_title, $_genre, $_runtime, $_actors, $_description)
+    public function __construct(string $_title, string $_genre, string $_runtime, string  $_actors, string $_description)
     {
         $this->title = $_title;
         $this->genre = $_genre;
@@ -17,4 +22,12 @@ class Movie
         $this->actors = $_actors;
         $this->description = $_description;
     }
-}
+    public function getType()
+    {
+    }
+};
+
+
+$Terminator = new Movie('Terminator', 'Azione', '120', 'Pippo', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo maiores ratione nulla asperiores qui. Fugiat temporibus deleniti, debitis dicta quasi saepe corrupti sequi doloribus labore quisquam nostrum aperiam officiis consequatur. ');
+
+var_dump($Terminator);
