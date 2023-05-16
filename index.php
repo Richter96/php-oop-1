b31b34401d471be15edbd850c0da7d2fd1c8b0b7
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-16 17:03:31 +0200

    add class genre

--- index.php
+++ index.php
@@ -2,32 +2,33 @@
 
 class Genre
 {
-    public $generi;
+    public $genre;
+    public function __construct(string $genre)
+    {
+        $this->genre = $genre;
+    }
 };
 
 class Movie
 {
-    public static $Type = 'Film';
     public $title;
     public $genre;
     public $runtime;
     public $actors;
     public $description;
 
-    public function __construct(string $_title, string $_genre, string $_runtime, string  $_actors, string $_description)
+    public function __construct(string $_title, string $_runtime, string  $_actors, string $_description, Genre $_genre)
     {
         $this->title = $_title;
-        $this->genre = $_genre;
         $this->runtime = $_runtime;
         $this->actors = $_actors;
         $this->description = $_description;
-    }
-    public function getType()
-    {
+        $this->genre = $_genre;
     }
 };
 
 
-$Terminator = new Movie('Terminator', 'Azione', '120', 'Pippo', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo maiores ratione nulla asperiores qui. Fugiat temporibus deleniti, debitis dicta quasi saepe corrupti sequi doloribus labore quisquam nostrum aperiam officiis consequatur. ');
+
+$Terminator = new Movie('Terminator', '120', 'Pippo', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo maiores ratione nulla asperiores qui. Fugiat temporibus deleniti, debitis dicta quasi saepe corrupti sequi doloribus labore quisquam nostrum aperiam officiis consequatur.', new Genre('azione'));
 
 var_dump($Terminator);
